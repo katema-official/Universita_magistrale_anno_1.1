1605e7291773331b53247eee98cceee60381dfa4
Katema <alessio.lagreca13@gmail.com>
2021-12-19 22:41:55 +0100

    Initial commit

---
+++ Sviluppo_software_in_gruppi_di_lavoro_complessi/esNuovoScript2.sh
@@ -0,0 +1,33 @@
+git init
+printf "start" > start.txt
+git add start.txt
+git commit -m "uno"
+git branch develop
+git checkout develop
+printf "\ncontinue" >> start.txt
+git add start.txt
+git commit -m "edit"
+git checkout master
+printf "new" > lib.txt
+git add lib.txt
+git commit -m "lib"
+git checkout -b trunk
+git cherry-pick develop
+git checkout develop
+mkdir back
+git mv start.txt back
+git commit -m "moved"
+git cherry-pick master
+
+git checkout HEAD^
+git reset trunk
+printf 
+
+
+
+
+git checkout trunk
+mkdir back
+git mv start.txt back
+printf "back" > .gitignore
+git branch -d master
