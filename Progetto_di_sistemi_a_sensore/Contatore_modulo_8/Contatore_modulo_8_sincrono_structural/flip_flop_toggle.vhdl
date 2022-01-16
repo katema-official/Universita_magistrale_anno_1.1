b020ac74ee4761d1b3dc4ade5bba42e52c680739
katema-official <alessio.lagreca13@gmail.com>
2022-01-16 11:51:41 +0100

    PSS: pulito codice flip_flop_toggle nel contatore strutturale
    Ora uso una variabile di tipo std_logic per aggiornare lo stato del flip_flop. Da notare che, se state è la mia variabile, io sono in grado di scrivere: state := not state.

--- Progetto_di_sistemi_a_sensore/Contatore_modulo_8/Contatore_modulo_8_sincrono_structural/flip_flop_toggle.vhdl
+++ Progetto_di_sistemi_a_sensore/Contatore_modulo_8/Contatore_modulo_8_sincrono_structural/flip_flop_toggle.vhdl
@@ -13,21 +13,13 @@ architecture arch of flip_flop_toggle is
 begin
 
 	toggle: process(clk)
-		variable state: integer := 0;
+		variable state: std_logic := '0';
 	begin
 		if(clk'event and clk = '0' and clk'last_value = '1')then	--se sono abilitato e c'è stato un fronte di discesa del clock, cambio
-			if state = 1 then
-				state := 0;
-			else
-				state := 1;
-			end if;
+			state := not state;
 		end if;
 		
-		if state = 1 then
-			ex <= '1';
-		else
-			ex <= '0';
-		end if;
+		ex <= state;
 		
 	end process toggle;
 
